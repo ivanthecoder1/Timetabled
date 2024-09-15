c4e2dfccb0233405d51db3f7976b1a2367f93b3c
Ivan Zhang <ivanzhang0111@gmail.com>
2024-09-15 13:40:48 -0400

    Finish lab 1 prework

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+	return (
+		<div className="App">
+			<h1>Timetabled 🕒</h1>
+			<h2>Plan your events</h2>
+			<Calendar />
+
+
+		</div>
+	)
+}
+
+export default App
